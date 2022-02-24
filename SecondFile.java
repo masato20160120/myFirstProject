068e6bc2e6368d917979794004395ec3def4e6a1
masato20160120 <87548648+masato20160120@users.noreply.github.com>
2022-02-24 19:42:05 +0900

    compareメソッドの実装

--- SecondFile.java
+++ SecondFile.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
+import com.sun.tools.javac.util.List;
+
 public class SecondFile{
   public static void main(String[] args){
     
-	  System.out.println("はじめてのpush!!");
+	  java.util.List<Integer> list = Arrays.asList(new Integer[] {1, 2, 3});
+	  list.sort((a, b) -> -a.compareTo(b));
+	  for(Integer numInteger : list) {
+		  System.out.println(num);
+	  }
   }
 }
